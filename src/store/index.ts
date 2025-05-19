f8b3ecfe95cba7402335a1d15fa5992177579da0
RBxRobson <robsonnbarbosa0@gmail.com>
2025-05-19 16:48:39 -0300

    feat: Adiciona novo reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import weatherApi from '../services/api'
 import cityReducer from './reducers/city'
+import dayReducer from './reducers/day'
 
 export const store = configureStore({
   reducer: {
     city: cityReducer,
+    day: dayReducer,
     [weatherApi.reducerPath]: weatherApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(weatherApi.middleware),
